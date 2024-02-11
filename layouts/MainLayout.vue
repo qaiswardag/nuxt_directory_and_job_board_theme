7e22d68599350cf7a223af7ae4c2a454d49c97da
Qais Wardag <qais.wardag@outlook.com>
2024-02-11 20:32:42 +0100

    Display fullscreen spinner on page load

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,7 +1,23 @@
 <script setup>
 import { ref, computed, watch, onMounted } from 'vue';
+import FullScreenSpinner from '../components/loaders/FullScreenSpinner.vue';
+import { delay } from '../helpers/delay';
+
+const isLoading = ref(false);
+
+onBeforeMount(() => {
+  isLoading.value = true;
+});
+
+onMounted(async () => {
+  await delay(500);
+  isLoading.value = false;
+});
 </script>
 
 <template>
+  <template v-if="isLoading">
+    <FullScreenSpinner></FullScreenSpinner>
+  </template>
   <slot />
 </template>
