885bd17393c4d23512c28b76660faf080ecce30a
Qais Wardag <qais.wardag@outlook.com>
2024-02-15 17:09:56 +0100

    Add headless ui

--- components/navbars/GuestsNavbar.vue
+++ components/navbars/GuestsNavbar.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import SlideOverPrimaryMenu from '../components/sidebars/SlideOverPrimaryMenu.vue';
 
 const route = useRoute();
 const routeName = route.name;
--- components/sidebars/SlideOverPrimaryMenu.vue
+++ components/sidebars/SlideOverPrimaryMenu.vue
@@ -1,1 +1,11 @@
+<script setup>
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue';
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
+</script>
 <template>Hiiii</template>
