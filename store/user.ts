58e1f54d8aa9e97d3df1985f6cfeb63f3a450159
Qais Wardag <qais.wardag@outlook.com>
2024-02-24 14:39:19 +0100

    Work on fetching user from backend

--- store/user.ts
+++ store/user.ts
@@ -1,9 +1,21 @@
 // stores/counter.ts
 
 import { defineStore } from 'pinia';
+import { vueFetch } from '../composables/vueFetch';
+
+const {
+  handleData: handleGetUser,
+  fetchedData: fetchedDataUser,
+  isError: isErrorUser,
+  error: errorUser,
+  errors: errorsUser,
+  isLoading: isLoadingUser,
+  isSuccess: isSuccessUser,
+} = vueFetch();
 
 export const useUserStore = defineStore('user', {
   state: () => ({
+    baseUrl: useRuntimeConfig().public.LARAVEL_APP_URL,
     user: {
       firstName: 'test1233',
       lastName: '1234rrrr',
@@ -13,9 +25,24 @@ export const useUserStore = defineStore('user', {
     getUser: (state) => state.user,
   },
   actions: {
-    fetchUser() {
-      console.log(`new user`);
+    getAppUrl(path: string) {
+      return this.baseUrl + '/' + path;
+    },
 
+    fetchUser() {
+      const path = 'api/loggedin-user';
+
+      console.log(`baseUrl:`, this.baseUrl);
+
+      this.getAppUrl(path);
+
+      //
+      //
+      //
+      //
+      //
+      //
+      // handleGetUser();
       this.user = {
         firstName: 'updated',
         lastName: 'new123',
