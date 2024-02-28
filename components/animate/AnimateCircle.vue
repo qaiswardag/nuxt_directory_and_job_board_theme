32d77cbcaac9a0f3b72552894e72a5c96b3d1ca0
Qais Wardag <qais.wardag@outlook.com>
2024-02-28 14:46:41 +0100

    Unique component for circle animation

---
+++ components/animate/AnimateCircle.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  bgColorLight: {
+    required: true,
+  },
+  bgColorLight: {
+    required: true,
+  },
+});
+</script>
+<template>
+  <span class="relative flex h-3 w-3">
+    <span
+      :class="[`${bgColorLight}`]"
+      class="animate-ping absolute inline-flex h-full w-full rounded-full opacity-75"
+    ></span>
+    <span
+      :class="[`${bgColorLight}`]"
+      class="relative inline-flex rounded-full h-3 w-3"
+    ></span>
+  </span>
+</template>
--- components/navbars/GuestsNavbar.vue
+++ components/navbars/GuestsNavbar.vue
@@ -114,14 +114,10 @@ const goToDashboard = function () {
 
         <!-- isErrorUser # start -->
         <template v-if="user && user.isLoadingUser">
-          <span class="relative flex h-3 w-3">
-            <span
-              class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"
-            ></span>
-            <span
-              class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"
-            ></span>
-          </span>
+          <AnimateCircle
+            bgColorLight="bg-green-600"
+            bgColorDark="bg-green-800"
+          ></AnimateCircle>
         </template>
         <!-- isErrorUser # end -->
         <!-- isErrorUser # start -->
