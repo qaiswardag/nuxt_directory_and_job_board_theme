901ae1301c9705e7cc6872a3c0623e99dbb2a8ac
Qais Wardag <qais.wardag@outlook.com>
2024-05-06 19:34:44 +0200

    Update design

--- components/footer/FooterGuests.vue
+++ components/footer/FooterGuests.vue
@@ -11,9 +11,7 @@ import ApplicationLogo from '../components/logos/ApplicationLogo.vue';
         <ApplicationLogo></ApplicationLogo>
       </div>
       <div class="lg:w-4/6 w-full mb-6">
-        <p class="myPrimaryParagraph font-medium lg:text-xl text-xs mb-4">
-          About
-        </p>
+        <p class="myPrimaryParagraph font-medium text-xl mb-4">About</p>
         <p class="myPrimaryParagraph lg:text-sm text-xs">
           Laravel, Vue, and Nuxt, a Page Builder, Listing Directory, Blog, and
           Job Board Theme. Allow your users to build listings, jobs or posts
@@ -21,9 +19,7 @@ import ApplicationLogo from '../components/logos/ApplicationLogo.vue';
         </p>
       </div>
       <div class="lg:w-2/6 w-full mb-6">
-        <p class="myPrimaryParagraph font-medium lg:text-xl text-xs mb-4">
-          Links
-        </p>
+        <p class="myPrimaryParagraph font-medium text-xl mb-4">Links</p>
         <div class="flex justify-center flex-col items-left myPrimaryGap mb-6">
           <p class="myPrimaryParagraph lg:text-sm text-xs">
             <a
