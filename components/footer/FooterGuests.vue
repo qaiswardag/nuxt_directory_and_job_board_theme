8abc2ad674dd64533b8bcf86d1ba4076d19ee9fc
Qais Wardag <qais.wardag@outlook.com>
2024-05-14 21:39:57 +0200

    Test

--- components/footer/FooterGuests.vue
+++ components/footer/FooterGuests.vue
@@ -49,7 +49,7 @@ import ApplicationLogo from '../components/logos/ApplicationLogo.vue';
               href="https://www.linkedin.com/in/qaiswardag"
               target="_blank"
             >
-              LinkedIn Qais Wardag
+              LinkedIn Qais Wardag test 1
             </a>
           </p>
         </div>
