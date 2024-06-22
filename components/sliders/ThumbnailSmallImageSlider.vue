ca557fdfc9e23acf1bf50c69902f14828ebe769b
Qais Wardag <qais.wardag@outlook.com>
2024-06-22 21:53:22 +0200

    Enhance CSS

--- components/sliders/ThumbnailSmallImageSlider.vue
+++ components/sliders/ThumbnailSmallImageSlider.vue
@@ -163,7 +163,7 @@ const sortedImages = computed(() => {
               'rounded-none h-full top-0 bottom-0 left-0 w-8': squareButtons,
             },
             {
-              'bg-opacity-60 top-[45%] left-[4px] shaddow h-10 w-10 rounded-full ':
+              'bg-opacity-50 top-[45%] left-[4px] shaddow h-10 w-10 rounded-full ':
                 !squareButtons,
             },
           ]"
@@ -181,7 +181,7 @@ const sortedImages = computed(() => {
               'rounded-none h-full top-0 right-0 bottom-0 w-8': squareButtons,
             },
             {
-              'bg-opacity-60 top-[45%] right-[4px] shaddow h-10 w-10 rounded-full ':
+              'bg-opacity-50 top-[45%] right-[4px] shaddow h-10 w-10 rounded-full ':
                 !squareButtons,
             },
           ]"
