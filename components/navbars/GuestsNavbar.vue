0dd8e881f1cd9ba1fe12fe052a49345734c87094
Qais Wardag <qais.wardag@outlook.com>
2025-03-16 11:52:44 +0100

    Update GuestsNavbar.vue to adjust header text size for improved readability

--- components/navbars/GuestsNavbar.vue
+++ components/navbars/GuestsNavbar.vue
@@ -76,7 +76,7 @@ onMounted(() => {
     @primaryMenuSlideOverButton="primaryMenuSlideOverButton"
   >
   </SlideOverPrimaryMenu>
-  <header class="w-full sm:text-sm text-xs">
+  <header class="w-full text-sm">
     <nav
       class="mx-auto flex myPrimaryGap items-center justify-end px-6 lg:px-8"
       aria-label="Global"
